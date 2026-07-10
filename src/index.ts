4f7466cb2130a0c69c61273f800eba104fb3f296
steverova <steverova0594@gmail.com>
2026-07-10 16:26:42 -0600

    added placeholders routes

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import "dotenv/config";
 import compression from "compression";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 import { env } from "#config/env";
 import { corsConfig } from "#middlewares/cors-config";
 import { errorHandler } from "#middlewares/error-handler";
 import { notFoundHandler } from "#middlewares/not-found";
 import { router } from "./router/index";
+import swaggerConfig from "#config/swagger";
 
 const app = express();
 
@@ -19,6 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(cookieParser());
 app.use(morgan("dev"));
+
+const swaggerSpec = swaggerJsdoc(swaggerConfig);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (_: Request, res: Response) => {
   res.send("Hello World!");
@@ -32,4 +38,5 @@ app.use(errorHandler);
 
 app.listen(env.PORT, () => {
   console.log(`App running on http://localhost:${env.PORT} 🚀`);
+  console.log(`Api docs http://localhost:${env.PORT}/docs `);
 });
